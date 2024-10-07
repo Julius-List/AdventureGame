f51fbad7d74e42d6d623e650a2a841043e45b04e
emilie1567 <emiliebrandstrup@gmail.com>
2024-10-07 13:22:26 +0200

    gjort switch klar i GameController

--- src/GameController.java
+++ src/GameController.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class GameController {
+    int input;
+    Scanner scan = new Scanner(System.in);
 
 public void start() {
+    input = scan.nextInt();
+    switch(input) {
+        case 1: //beach
+
+        case 2: //sea
+
+        case 3: //jungle
+
+        default:
+            System.out.println("You can only choose between: \n1: beach, 2: sea or 3: jungle.");
+            break;
 
     }
+}
 }
