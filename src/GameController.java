085c887327c2c91f2f582388428b3ec354f57070
emilie1567 <emiliebrandstrup@gmail.com>
2024-10-07 13:05:18 +0200

    Slettet game

--- src/GameController.java
+++ src/GameController.java
@@ -1,2 +1,6 @@
 public class GameController {
+
+public void start() {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
 
+    public static void main(String[] args) {
+        GameController game = new GameController();
+        game.start();
+    }
 }
+
