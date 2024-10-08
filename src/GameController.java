8b303f8a626d4f72a57c025c9ec100021f44598c
emilie1567 <emiliebrandstrup@gmail.com>
2024-10-08 14:06:31 +0200

    Tilføjet første blok af valgmuligheder i Jungle, recursion i default switches (i dem vi indtil videre bruger) samt tilføjet startbesked.

--- src/GameController.java
+++ src/GameController.java
@@ -3,29 +3,41 @@ import java.util.Scanner;
 public class GameController {
     Scanner scan = new Scanner(System.in);
     int input;
+    boolean isFirstGame = true; // Laver en variabel som tjekker om det er første gang spilleren starter programmet.
 
 public void start() {
-    System.out.println("You wake up on the beach of a deserted island blablabla. \n1: Stay on the beach.\n2: Walk to the sea.\n3: Walk to the jungle.");
+    if (isFirstGame) {
+    System.out.println("You wake up on the beach of a deserted island blablabla."); // startbesked.
+    isFirstGame = false; // sætter isFirstGame til false efter, så man ikke får startbeskeden når man returnerer til start.
+
+    }
+    // Viser choice options.
+    System.out.println("1: Stay on the beach.");
+    System.out.println("2: Walk to the sea.");
+    System.out.println("3: Walk to the jungle.");
 
     input = scan.nextInt();
-    switch(input) { //switch
-        case 1: //kalder instance af beach
+
+    switch(input) { // switch
+        case 1: // kalder instance af beach.
             Beach beachPath = new Beach();
             beachPath.enter();
             break;
 
-        case 2: //kalder instance af sea
+        case 2: // kalder instance af sea.
             Sea seaPath = new Sea();
             seaPath.enter();
             break;
 
-        case 3: //kalder instance af jungle
+        case 3: // kalder instance af jungle.
             Jungle junglePath = new Jungle();
-            junglePath.enter();
+            junglePath.enter(this);
             break;
 
+        // default case ved invalid choice der genkalder metoden igen (rekursion) så man kan vælge igen.
         default:
             System.out.println("You can only choose 1, 2, or 3.");
+            start();
             break;
 
          }
--- src/Jungle.java
+++ src/Jungle.java
@@ -4,28 +4,41 @@ public class Jungle {
     Scanner scan = new Scanner(System.in);
     int input;
 
-    public void enter() {
-        input = scan.nextInt();
+    public void enter(GameController gameController) { // Så vi kan bruge GameController instance
         System.out.println("You walk towards the jungle...");
+        firstChoice(gameController); // kalder firstChoice med gameController som parameter.
+
+    }
+
+    private void firstChoice(GameController gameController) { // bruger gameController så vi kan calle start i option 1.
         System.out.println("As you step into the jungle, you feel like you are being watched.");
-        System.out.println("1: Do you go back?\n2: Do you turn right?\n3: Do you turn left?");
+        System.out.println("1: Do you go back?");
+        System.out.println("2: Do you turn right?");
+        System.out.println("3: Do you turn left?");
+
+        int input = scan.nextInt();
+
 
         switch (input) {
             case 1:
-
+                System.out.println("You can't shake the feeling that something is wrong. You head back to the beach.");
+                gameController.start(); // Returnerer til start.
                 break;
 
             case 2:
-
+                System.out.println("You decide to take the path to your right. You stumble upon a tree bearing fruits.");
                 break;
 
             case 3:
-
+                System.out.println("As you turn left, you trip over a branch and tumble down a mudslide. (You find a lighter) How convenient! Walk back to the beach or continue?");
                 break;
 
+            // default case ved invalid choice der genkalder metoden igen (rekursion) så man kan vælge igen.
             default:
                 System.out.println("You can only choose 1, 2, or 3.");
+                firstChoice(gameController);
                 break;
 
         }
+    }
 }
